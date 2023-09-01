24297fb3af022253ba2cf98d664404535e7e123a
patnaikarnav <20BCS6658@cuchd.in>
2023-09-01 23:25:30 +0530

    logging added for or model

--- or.py
+++ or.py
@@ -1,30 +1,44 @@
 from utils.all_utils import prepare_data, save_plot
 import pandas as pd
 from utils.model import Perceptron
+import os
+import logging
+
+gate = "OR gate"
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_dir, "running_logs.log"),
+                    level=logging.INFO,
+                    format='[%(asctime)s: %(levelname)s: %(module)s]: %(message)s',
+                    filemode='a')
 
 def main(data, modelName, plotName, eta, epochs):
-    df_OR = pd.DataFrame(OR)
-
-    X, y = prepare_data(df_OR)
+    df = pd.DataFrame(data)  # Use the 'data' parameter, not 'OR'
+    logging.info(f"This is the raw dataset:\n {df}")
+    X, y = prepare_data(df)
 
     model_or = Perceptron(eta=eta, epochs=epochs)
-    model_or.fit(X,y)
+    model_or.fit(X, y)
 
-    _ = model_or.total_loss() # _  =dummy value / wont be used
+    _ = model_or.total_loss()  # _ = dummy value / won't be used
 
-    model_or.save(filename=modelName,model_dir="C:/Users/Arnav/Desktop/Data Analyst/INEURON/Untitled Folder/model")
+    model_or.save(filename=modelName, model_dir="C:/Users/Arnav/Desktop/Data Analyst/INEURON/Untitled Folder/model")
 
-    save_plot(df_OR, model_or, filename=plotName)
+    save_plot(df, model_or, filename=plotName)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     OR = {
-        "x1": [0,0,1,1],
-        "x2": [0,1,0,1],
-        "y": [0,1,1,1]
+        "x1": [0, 0, 1, 1],
+        "x2": [0, 1, 0, 1],
+        "y": [0, 1, 1, 1]
     }
-    ETA=0.3
-    EPOCHS=10
-    main(data=OR, modelName="or.model", plotName="or.png", eta=ETA, epochs=EPOCHS)
+    ETA = 0.3
+    EPOCHS = 10
     
-
-
+    try:
+        logging.info(f">>>>> start training for {gate} >>>>>")
+        main(data=OR, modelName="or.model", plotName="or.png", eta=ETA, epochs=EPOCHS)
+        logging.info(f"<<<< done training for {gate} <<<<\n\n")
+    except Exception as e:
+        logging.exception(e)
+        raise e
